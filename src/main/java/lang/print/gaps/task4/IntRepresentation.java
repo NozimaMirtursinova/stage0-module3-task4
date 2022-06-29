67ec0570de4cea7b19af3b354640959bd999f459
Nozima Mirtursinova <Nozimamirtursinova>
2022-06-29 13:33:05 +0500

    Merge remote-tracking branch 'origin/main' into main

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -4,11 +4,15 @@ public class IntRepresentation {
     public static void main(String[] args) {
         char firstLetter = 'c';
         int c = firstLetter;
+        System.out.println(c);
         char secondLetter = 'h';
         int h = secondLetter;
+        System.out.println(h);
         char thirdLetter = 'a';
         int a = thirdLetter;
+        System.out.println(a);
         char forthLetter = 'r';
         int r = forthLetter;
+        System.out.println(r);
     }
 }
